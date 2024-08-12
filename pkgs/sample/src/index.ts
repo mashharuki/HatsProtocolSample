466ef66a9f438efdeba0daab12e29b9e9a1259cb
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2024-08-12 05:57:58 +0000

    hatの情報が取得できるように改良しました。

--- pkgs/sample/src/index.ts
+++ pkgs/sample/src/index.ts
@@ -2,6 +2,7 @@ import {createPublicClient, createWalletClient, formatEther, http} from "viem";
 import {sepolia} from "viem/chains";
 import * as dotenv from "dotenv";
 import {privateKeyToAccount} from "viem/accounts";
+import {HatsClient} from "@hatsprotocol/sdk-v1-core";
 
 dotenv.config();
 
@@ -24,6 +25,13 @@ const walletClient = createWalletClient({
   account,
 });
 
+// HatsProtocol用のインスタンスを生成する。
+const hatsClient = new HatsClient({
+  chainId: sepolia.id,
+  publicClient: client,
+  walletClient,
+});
+
 // クライアントの使用例
 async function main() {
   try {
@@ -32,7 +40,15 @@ async function main() {
     console.log("Current Block Number:", blockNumber);
     // ウォレットの残高を取得する
     const balance = await client.getBalance({address: account.address});
-    console.log(`Balance of ${account.address}: ${formatEther(balance)} ETH`);
+    console.log(`Balance: ${formatEther(balance)} ETH`);
+
+    console.log("hatsClient:", hatsClient.chainId);
+
+    console.log("getting hat info");
+
+    const hatInfo = await hatsClient.viewHat(BigInt(442));
+
+    console.log(hatInfo);
   } catch (error) {
     console.error("Error:", error);
   }
