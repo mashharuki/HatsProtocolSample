f13508b78e34d194c29f192a357ce562ff6eead3
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-08-23 14:02:33 +0900

    リレイヤーロジックを追加

--- sample-frontend/lib/constants.ts
+++ sample-frontend/lib/constants.ts
@@ -1,2 +1,5 @@
 // MODULES ここでHatのModule名を指定している。
 export const CLAIMS_HATTER_MODULE_NAME = 'Multi Claims Hatter'
+
+export const BASE_API_URL = "localhost:3000";
+export const FORWARDER_CONTRACT_ADDRESS = "";
